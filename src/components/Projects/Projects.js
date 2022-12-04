33506fbc6ef509dabeabe33f4f8b09469d1582e2
Sasha Rodriguez Carlomagno <caballerosasha@hotmail.com>
2022-12-04 18:12:53 -0300

    added project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,7 +15,10 @@ const Projects = ({projectsRef}) => {
                 text={`E-commerce realizado con ReactJS y la Dbass Firebase Firestore. 
                 Tienda de venta de instrumentos musicales.`} url={'https://github.com/SashaShadow/rodriguez-carlomagno-win-guitars'}
                 url2={'http://win-guitars.vercel.app/'}/>
-                <ProjectCard image={'https://res.cloudinary.com/ddi5wmffy/image/upload/v1659796618/alice_jjtsgs.png'}
+                <ProjectCard image={'https://res.cloudinary.com/ddi5wmffy/image/upload/v1670187909/Captura_desde_2022-12-04_18-02-47_qr9lpl.png'}
+                text={'E-commerce (fullstack app) realizado con Node, Express.js, MongoDB, Passport (APIS y Backend) y EJS como motor de plantillas.'} url={'https://github.com/SashaShadow/tetsu-fragances'}
+                url2={'#'}/>
+                <ProjectCard image={'https://res.cloudinary.com/ddi5wmffy/image/upload/v1659796618/alice_jjtsgs.png'} 
                 text={`Landing page para diseñadora gráfica. Tecnologías utilizadas: 
                 ReactJS para la interfaz de usuario, consumiendo una API que brinda 
                 la información de los proyectos realizados por la autora.`} url={'https://github.com/SashaShadow/alice'}
